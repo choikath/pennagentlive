80445f983708d86e99f84a76fe5d6e36b750915d
choikath <choikath@gmail.com>
2016-06-06 13:13:37 -0400

    homescreen app how-to popup

--- js/creative.js
+++ js/creative.js
@@ -42,5 +42,20 @@
         }
     })
 
+	// Pop-up functionality
+	$(function() { //document ready
+		$(".displayPopup").on("click", function() { //click event of the button
+			$("#overlay").fadeTo(100, .3); //this will show the overlay awith a crossbrowser transparency
+			$("#popup1").show(); // show popup1
+			return false; //disable event propogation (avoid addition of "#" to url)
+		})
+ 
+		$(".popup .close").on("click", function() { //click event of the close button in popup
+			$("#overlay").hide(); //hide overlay
+			$(".popup").hide(); //hide popup
+			return false; //disable event propogation (avoid addition of "#" to url)
+		})
+	})
+
 
 })(jQuery); // End of use strict
